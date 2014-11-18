94fe6515e3801ea7522172c9bac93361c9ea58ef
Valentin <valio26@email.bg>
2014-11-18 11:10:58 +0200

    cpp file added

---
+++ 123.cpp
@@ -0,0 +1,15 @@
+#include "stdafx.h"
+#include <iostream> 
+
+using namespace std;
+
+int main()
+{
+	double a = 2;
+	double b = 5;
+	double p;
+	p = 2 * (a + b);
+	cout << "p= " << p << endl;
+	return 0;
+}
+
